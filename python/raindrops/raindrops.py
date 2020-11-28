c4ef157024f3eb2a46ca9a826a8501f6aa537e87
hawk-praxs <pratyush.11708251@lpu.in>
2020-11-28 10:25:08 +0530

    Raindrops exercise
    Nearly solved the exercise. Need to solve the no factor portion.

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,9 @@
 def convert(number):
-    pass
+    sound_dict = {3: 'Pling', 5: 'Plang', 7: 'Plong'}
+    sound_list = [str(number)]
+    
+    for i in sound_dict.keys():
+        if number%i == 0:
+            sound_list.append(sound_dict[i])
+    
+    return ''.join(sound_list)
